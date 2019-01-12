314b3ccd9f8d00c84d8cab6f09f8771ef782ce29
TedWildenradt <tedkwildenradt@gmail.com>
2019-01-12 13:27:38 -0800

    Add axis to chart

--- barchart2.js
+++ barchart2.js
@@ -9,24 +9,49 @@ d3.csv("City_MedianRentalPrice_2Bedroom.csv", function(data) {
     if (d1.RegionName === 'San Francisco'){
       console.log(d1)
       let rental2 = Object.values(d1).slice(12)
+      console.log(rental2)
+      let rentalTimes = Object.keys(d1).slice(12);
+      console.log(rentalTimes)
 
-      let height1 = 500
-      let width1 = 700
+      let margin = {left: 100, right: 10, top: 10, bottom: 100}
+
+      let height1 = 600 - margin.top - margin.bottom
+      let width1 = 900 - margin.left - margin.right
       let barPadding = 1
 
       let y = d3.scaleLinear()
               .domain([d3.min(rental2) - 200, d3.max(rental2)])
               .range([0, height1])
+              
 
       let x = d3.scaleTime()
               .domain([new Date(2010, 0, 1), new Date(2019, 0, 1)])
               .range([0, width1])
-    
+      
+      let x2 = d3.scaleBand()
+              .domain(rentalTimes)
+              .range([0, width1])
+
       let svg1 = d3.select("#chart2").append('svg')
-                .attr("width", width1)
-                .attr('height', height1)
-    
-      let rects = svg1.selectAll('rect')
+                .attr("width", width1 + margin.left + margin.right)
+                .attr('height', height1 + margin.top + margin.bottom)
+
+      let g = svg1.append('g')
+              .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')')
+      
+      // Axis for left and bottom
+      let xAxisCall = d3.axisBottom(x);
+      g.append('g')
+          .attr('class','x-axis')
+          .attr('transform', 'translate(0, ' + height1 + ')')
+          .call(xAxisCall)
+
+      let yAxisCall = d3.axisLeft(y);
+      g.append('g')
+          .attr('class','y-axis')
+          .call(yAxisCall)
+
+      let rects = g.selectAll('rect')
                     .data(rental2)
     
       rects.enter()
